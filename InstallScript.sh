f47b8968a72d47afc5973d204e231e5378b0b1de
Alex Hanson <ahanson@runbox.com>
2018-04-09 09:59:50 -0500

    Update InstallScript.sh
    remove spaces in install_syncthing

--- InstallScript.sh
+++ InstallScript.sh
@@ -158,10 +158,8 @@ sudo dpkg -i sendanywhere_latest_amd64.deb
 function install_syncthing {
 # Add the release PGP keys:
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-
 # Add the "stable" channel to your APT sources:
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
 # Update and install syncthing:
 sudo apt-get update
 sudo apt-get -y install syncthing
