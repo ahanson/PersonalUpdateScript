abeb2172dc95cf6b30e3fce111f1b6e4c641012b
Alex Hanson <ahanson@runbox.com>
2019-01-01 11:16:11 -0600

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -107,7 +107,6 @@ done
 	}
 
 function install_chromebrowser {
-# Install Chrome Browser:
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
